a43ab62c6bc8b7ba5f898c19dac13c7ac8dff0bf
FelipeMartins <felipem.silva@viannasempre.com.br>
2021-04-18 23:24:10 -0300

    Projeto Buffet da aula de PWA

---
+++ BuffetFelipe/Models/Buffet/Cliente/ClienteEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BuffetFelipe.Models.Buffet.Cliente
+{
+    public class ClienteEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        
+    }
+}
---
+++ BuffetFelipe/Models/Buffet/Cliente/ClienteService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuffetFelipe.Models.Buffet.Cliente
+{
+    public class ClienteService
+    {
+        public List<ClienteEntity> obterClientes()
+        {
+            var listaDeClientes = new List<ClienteEntity>();
+
+            listaDeClientes.Add(new ClienteEntity
+            {
+                Id = 1,
+                Nome = "Felipe",
+                DataDeNascimento = new DateTime(1989,11,1)
+            });
+            
+            listaDeClientes.Add(new ClienteEntity
+            {
+                Id = 2,
+                Nome = "José",
+                DataDeNascimento = new DateTime(1977,1,10)
+            });
+            
+            return listaDeClientes;
+        }
+    }
+}
